d1de6ad7d3ab71f74a83f22435af2a1ade1ff2b4
winstonkaranja <ngigewinston@gmail.com>
2023-02-14 14:11:20 +0300

    nav commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      hello world
+    </div>
+  )
+}
+
+export default App
